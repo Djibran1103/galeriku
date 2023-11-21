7c3eb4ddc5ff56d3c5704fbcedb0e4e4bfb7a896
Djibran1103 <11211070@student.itk.ac.id>
2023-11-21 17:04:34 +0800

    Tailwind

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    return ( 
+        <nav>
+            <h1 className="bg-red-500 text-6xl font-bold">
+                Selamat Ngerjain wkwkwkwkwk
+            </h1>
+        </nav>
+     );
+}
+ 
+export default Navbar;
