a46a47ebc6c8edc1726810d380118cfecb5e6574
AriqH <89585448+AriqH@users.noreply.github.com>
2023-11-27 19:18:51 +0800

    card

---
+++ frontend/src/components/Card.jsx
@@ -0,0 +1,111 @@
+const Card = () => {
+  return (
+    <div className="mx-2 mb-5">
+      <h1 className="text-center pt-8 pb-8 font-bold text-4xl">
+        Check The New Arrival
+      </h1>
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+        <a href="#">
+          <img
+            className="h-auto max-w-full rounded-lg"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg"
+            alt=""
+          />
+        </a>
+        <a href="#">
+          <img
+            className="h-auto max-w-full rounded-lg"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
+            alt=""
+          />
+        </a>
+        <a href="#">
+          <img
+            className="h-auto max-w-full rounded-lg"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg"
+            alt=""
+          />
+        </a>
+        <a href="#">
+          <img
+            className="h-auto max-w-full rounded-lg"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg"
+            alt=""
+          />
+        </a>
+      </div>
+      <div>
+        <h1 className="text-center pt-8 pb-8 font-bold text-4xl">
+          Check The New Arrival
+        </h1>
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+          <a href="#">
+            <img
+              className="h-auto max-w-full rounded-lg"
+              src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg"
+              alt=""
+            />
+          </a>
+          <a href="#">
+            <img
+              className="h-auto max-w-full rounded-lg"
+              src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg"
+              alt=""
+            />
+          </a>
+          <a href="#">
+            <img
+              className="h-auto max-w-full rounded-lg"
+              src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg"
+              alt=""
+            />
+          </a>
+          <a href="#">
+            <img
+              className="h-auto max-w-full rounded-lg"
+              src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg"
+              alt=""
+            />
+          </a>
+        </div>
+      </div>
+      <div>
+        <h1 className="text-center pt-8 pb-8 font-bold text-4xl	">
+          Check The New Arrival
+        </h1>
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+          <a href="#">
+            <img
+              className="h-auto max-w-full rounded-lg"
+              src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg"
+              alt=""
+            />
+          </a>
+          <a href="#">
+            <img
+              className="h-auto max-w-full rounded-lg"
+              src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg"
+              alt=""
+            />
+          </a>
+          <a href="#">
+            <img
+              className="h-auto max-w-full rounded-lg"
+              src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg"
+              alt=""
+            />
+          </a>
+          <a href="#">
+            <img
+              className="h-auto max-w-full rounded-lg"
+              src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg"
+              alt=""
+            />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,16 +1,16 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import Card from "../components/Card";
 
 const Home = () => {
-    return ( 
-        <div>
-            <Navbar />
-            <div className="justify-center text-center">
-                Disini Letak Banner
-            </div>
-            <Footer />
-        </div>
-     );
-}
- 
+  return (
+    <div>
+      <Navbar />
+      <div className="justify-center text-center">Disini Letak Banner</div>
+      <Card />
+      <Footer />
+    </div>
+  );
+};
+
 export default Home;
