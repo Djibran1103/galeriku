7dadb606c4504c2c7b8300af6321d9e8ccf3d482
Djibran1103 <11211070@student.itk.ac.id>
2023-11-27 14:36:25 +0800

    Footer, URL masih random

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,15 @@
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const Home = () => {
     return ( 
-        <Navbar />
+        <div>
+            <Navbar />
+            <div className="justify-center text-center">
+                Disini Letak Banner
+            </div>
+            <Footer />
+        </div>
      );
 }
  
